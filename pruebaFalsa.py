dcee52c3bc6c1394e04de0778b0c93be372e81c9
danilodiez <danilodiez1@hotmail.com>
2020-06-17 15:55:50 -0300

    funcion falsa intervalo llegada

--- pruebaFalsa.py
+++ pruebaFalsa.py
@@ -17,3 +17,21 @@ def GeneraTiempo2():
     else: 
         respuesta = 6
     return respuesta
+
+def GeneraIntervaloLlegada2():
+    aleatorio=0
+    respuesta = 0
+    aleatorio = random.random()
+    if(aleatorio <=0.015):
+        respuesta = 4
+    elif(aleatorio<=0.1):
+        respuesta = 6
+    elif(aleatorio<=0.25):
+        respuesta = 7
+    elif(aleatorio<=0.30):
+        respuesta = 8
+    elif(aleatorio<=0.60):
+        respuesta = 9
+    else: 
+        respuesta = 10
+    return respuesta
