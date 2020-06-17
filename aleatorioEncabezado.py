2cec7671d75e796d2ebce8ebe668478612ba7eaf
FacuCuzziol <facucuzziol@gmail.com>
2020-06-17 15:53:31 -0300

    generar t procesamiento

--- aleatorioEncabezado.py
+++ aleatorioEncabezado.py
@@ -19,3 +19,54 @@ for x in range(100):
 print(contA)
 print(contB)
 print(contC)
+
+# Codigo simulacion
+
+def GeneraTiempo():
+    # el tiempo de procesamiento puede ser entre 1 - 5 
+    # (con 2 decimales)
+    return round(random.uniform(0,5),2)
+     
+
+def GeneraLugar():
+
+
+def GeneraSiguiente():
+    
+
+estadoTerminalA = False;
+estadoTerminalB = False;
+estadoTerminalC = False;
+
+tiempoRemanenteA = 0;
+tiempoRemanenteB = 0;
+tiempoRemanenteC = 0;
+
+memoriaRouterA = False;
+memoriaRouterB = False;
+memoriaRouterC = False;
+
+condicion = true
+tiempo = 0
+
+
+while condicion:
+    if tiempo == tiempoRemanenteA and 
+    procesamientoPaqueteActual= GeneraTiempo2() 
+    destinoPaqueteActual = GeneraLugar2() #aleatorioEncabezado
+    intervaloLlegada =GeneraIntervaloLlegada2()
+
+    if procesamientoPaqueteActual == 1:
+        if estadoTerminalA == False:
+            estadoTerminalA = True
+            tiempoRemanenteA = procesamientoPaqueteActual
+
+        else:
+            mem
+
+
+    
+    
+    
+    
+
